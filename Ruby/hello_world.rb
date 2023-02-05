b2dc4f343dc21507e631dd3ae40bb4474acbd7c3
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-02-05 21:25:04 +0300

    data types and methods in ruby

---
+++ Ruby/hello_world.rb
@@ -0,0 +1,29 @@
+puts 'hello world!'
+
+#numbers
+#integer operations return an integer
+# 17/5 = 3
+# 17/5.0 = 3.4
+
+#check evenness
+ puts 3.even?
+ puts 5.odd?
+
+ #strings
+ puts "Welcome " + "to " + "Odin"
+ puts "Welcome ".concat("to ").concat("Odin!")
+
+ #substrings
+ puts "hello"[0]
+ puts "hello"[0..2]
+ puts "hello"[-1]
+
+ #interpolation
+ name = "Odin"
+ puts "Hello, #{name}"
+ #single quotes don't work
+ puts 'Hello, #{name}'
+
+ #sub
+
+ "heggo".gsub("g", "l")
