b6c4c7935b8daed2d4714477527ebe0ed1200fff
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-02-06 11:53:18 +0300

    arrays

---
+++ Ruby/arrays.rb
@@ -0,0 +1,17 @@
+nums = [1,2,3,4,5]
+
+# nums.push(6,7)
+# nums.shift
+
+# nums.unshift(10)
+# puts nums
+
+a = [1,2,3,4]
+b = [5,6,7,8]
+
+# puts a + b
+# puts a.concat(b)
+
+c = a.concat(b)
+#puts c
+
