1f64b878c6187ce0df2ea417fcc7addbf54e8093
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-02-07 13:40:27 +0300

    methods in ruby

--- Ruby/methods.rb
+++ Ruby/methods.rb
@@ -1,37 +1,37 @@
-# # # # # puts "anything".reverse
+puts "anything".reverse
 
-# # # # def greet(name = "John")
-# # # #     "Hello, #{name}!"
-# # # # end
+def greet(name = "John")
+    "Hello, #{name}!"
+end
 
-# # # # puts greet()
-# # # # puts greet("Kevin")
+puts greet()
+puts greet("Kevin")
 
 
-# # # def even_odd(num) 
-# # #     #explicit return
-# # #     unless num.is_a? Numeric
-# # #         return "This is not a number"
-# # #     end
-# # #     #implicit returns
-# # #     if num % 2 == 0
-# # #         "This is an even number"
-# # #     else
-# # #         "This is an odd number"
-# # #     end
-# # # end
+def even_odd(num) 
+    #explicit return
+    unless num.is_a? Numeric
+        return "This is not a number"
+    end
+    #implicit returns
+    if num % 2 == 0
+        "This is an even number"
+    else
+        "This is an odd number"
+    end
+end
 
-# # # puts even_odd("a")
+# puts even_odd("a")
 
-# # phrase = "madam"
+phrase = "madam"
 
-# # puts phrase.split("").reverse.join("").upcase
+puts phrase.split("").reverse.join("").upcase
 
-# #predicate methods : return a boolean response
+#predicate methods : return a boolean response
 
-# puts 5.even?
-# puts 6.odd?
-# puts 13.between?(10, 15)
+puts 5.even?
+puts 6.odd?
+puts 13.between?(10, 15)
 
 #bang methods: overwrite the original value
 
