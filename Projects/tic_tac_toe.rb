58bb9342207fd849445e43bb62dcbae6bb3191ae
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-02-08 11:49:13 +0300

    tic tac toe game in ruby

---
+++ Projects/tic_tac_toe.rb
@@ -0,0 +1,37 @@
+puts 'Welcome to Tic Tac Toe!'
+
+board = [" ", " ", " ", " ", " ", " ", " "]
+play(board)
+
+
+def display_board(board) 
+    puts " # | # | # "
+    puts "-----------"
+    puts " # | # | # "
+    puts "-----------"
+    puts " # | # | # "
+end
+
+def input_to_index(user_input)
+    user_input.to_i - 1
+end
+
+def player_move(board, index, marker)
+    board[index] = marker
+end
+
+def position_taken? (board, index)
+    if board[index] == "" || board[index] == " " || board[index] == nil
+        return false
+    else
+        return true
+    end
+end
+
+def valid_move? (board, index)
+    if !position_taken? (board, index) && (index).between?(0,8)
+        return true
+    else
+        return false
+    end
+end
